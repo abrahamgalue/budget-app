a05a1171863598dc915fff3658640779b61b2a70
Abraham Galue <abrahamgalue512@gmail.com>
2023-10-24 17:40:16 -0400

    docs: functions helpers

---
+++ src/helpers.js
@@ -0,0 +1,9 @@
+// Local Storage
+export const fetchData = (key) => {
+  return JSON.parse(localStorage.getItem(key))
+}
+
+// delete item
+export const deleteItem = ({ key }) => {
+  return localStorage.removeItem(key)
+}
