975b0bc8df150d304bde4255d63225566e602929
Abraham Galue <abrahamgalue512@gmail.com>
2023-10-24 17:39:09 -0400

    feat: create router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,38 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+// Layouts
+import Main, { mainLoader } from './layouts/Main'
+
+// Actions
+import { logoutAction } from './actions/logout'
+
+// Routes
+import DashBoard, { dashboardLoader } from './pages/Dashboard'
+import Error from './pages/Error'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main />,
+    loader: mainLoader,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <DashBoard />,
+        loader: dashboardLoader,
+        errorElement: <Error />,
+      },
+      {
+        path: 'logout',
+        action: logoutAction,
+      },
+    ],
+  },
+])
+
 function App() {
-  return (
-    <>
-      <h1>Budget App</h1>
-    </>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App
