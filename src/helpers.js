b5db149f6a338c5e8818f5e5becd6de1e267e8fb
Abraham Galue <abrahamgalue512@gmail.com>
2023-10-26 12:01:54 -0400

    feat: create budget and random color

--- src/helpers.js
+++ src/helpers.js
@@ -1,6 +1,26 @@
+const generateRandomColor = () => {
+  const existingBudgetsLength = fetchData('budgets')?.length ?? 0
+  return `${existingBudgetsLength * 34} 65% 50%`
+}
+
 // Local Storage
 export const fetchData = (key) => {
   return JSON.parse(localStorage.getItem(key))
+}
+
+// create budget 
+export const createBudget = ({ name, amount }) => {
+  const newItem = {
+    id: crypto.randomUUID(),
+    name: name,
+    createdAt: Date.now(),
+    amount: Number(amount),
+    color: generateRandomColor()
+  }
+
+  const existingBudgets = fetchData('budgets') ?? []
+
+  return localStorage.setItem('budgets', JSON.stringify([...existingBudgets, newItem]))
 }
 
 // delete item
