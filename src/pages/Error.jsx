ba1c37f8996cef127a1362df5ae3e07b1550284a
Abraham Galue <abrahamgalue512@gmail.com>
2023-10-26 11:18:17 -0400

    feat: custom error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,28 @@
+import { useRouteError, Link, useNavigate } from 'react-router-dom'
+
+// library imports
+import { HomeIcon, ArrowUturnLeftIcon } from '@heroicons/react/24/solid'
+
 const Error = () => {
-  return <div>Error</div>
+  const error = useRouteError()
+  const navigate = useNavigate()
+
+  return (
+    <div className='error'>
+      <h1>Uh oh! We've got a problem.</h1>
+      <p>{error.message || error.statusText}</p>
+      <div className='flex-md'>
+        <button className='btn btn--dark' onClick={() => navigate(-1)}>
+          <ArrowUturnLeftIcon widht={20} />
+          <span>Go Back</span>
+        </button>
+        <Link to='/' className='btn btn--dark'>
+          <HomeIcon widht={20} />
+          <span>Go home</span>
+        </Link>
+      </div>
+    </div>
+  )
 }
 
 export default Error
