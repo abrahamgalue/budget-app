135ba65b94cdd91ff6ee19a9e1cd4988512ba13c
Abraham Galue <abrahamgalue512@gmail.com>
2023-10-25 21:44:02 -0400

    feat: adding a login form

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,12 @@
 // rrd imports
 import { useLoaderData } from 'react-router-dom'
 
+// library imports
+import { toast } from 'react-toastify'
+
+// components
+import Intro from '../components/Intro'
+
 // helper functions
 import { fetchData } from '../helpers'
 
@@ -10,15 +16,22 @@ export function dashboardLoader() {
   return { userName }
 }
 
+// action
+export async function dashboardAction({ request }) {
+  const data = await request.formData()
+  const formData = Object.fromEntries(data)
+  try {
+    localStorage.setItem('userName', JSON.stringify(formData.userName))
+    return toast.success(`Welcome ${formData.userName}!`)
+  } catch (e) {
+    throw new Error('There was a problem creating your account.')
+  }
+}
+
 const DashBoard = () => {
   const { userName } = useLoaderData()
 
-  return (
-    <div>
-      <h1>{userName}</h1>
-      DashBoard
-    </div>
-  )
+  return <>{userName ? <p>{userName}</p> : <Intro />}</>
 }
 
 export default DashBoard
