2e504e5da1cd8dd2dae58f8ce7b8e4bcc8403895
Abraham Galue <abrahamgalue512@gmail.com>
2023-10-26 12:04:50 -0400

    refactor: handling multiple form submissions

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -26,6 +26,7 @@ export const Intro = () => {
             aria-label='Your Name'
             autoComplete='given-name'
           />
+          <input type='hidden' name='_action' value='newUser' />
           <button type='submit' className='btn btn--dark'>
             <span>Create Account</span>
             <UserPlusIcon width={20} />
