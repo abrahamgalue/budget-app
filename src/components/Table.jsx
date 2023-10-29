50afd7b9f3c43ca2e17a81bc4363930ccdbf0ce3
Abraham Galue <abrahamgalue512@gmail.com>
2023-10-29 18:46:42 -0400

    feat: add expenses table

---
+++ src/components/Table.jsx
@@ -0,0 +1,29 @@
+// component import
+import ExpenseItem from './ExpenseItem'
+
+const Table = ({ expenses }) => {
+  const tableHeaders = ['Name', 'Amount', 'Date']
+
+  return (
+    <div className='table'>
+      <table>
+        <thead>
+          <tr>
+            {tableHeaders.map((item, index) => (
+              <th key={index}>{item}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map(expense => (
+            <tr key={expense.id}>
+              <ExpenseItem expense={expense} />
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Table
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -9,6 +9,7 @@ import Intro from '../components/Intro'
 import AddBudgetForm from '../components/AddBudgetForm'
 import AddExpenseForm from '../components/AddExpenseForm'
 import BudgetItem from '../components/BudgetItem'
+import Table from '../components/Table'
 
 // helper functions
 import { createBudget, createExpense, fetchData, waait } from '../helpers'
@@ -17,8 +18,9 @@ import { createBudget, createExpense, fetchData, waait } from '../helpers'
 export function dashboardLoader() {
   const userName = fetchData('userName')
   const budgets = fetchData('budgets')
+  const expenses = fetchData('expenses')
 
-  return { userName, budgets }
+  return { userName, budgets, expenses }
 }
 
 // action
@@ -68,7 +70,7 @@ export async function dashboardAction({ request }) {
 }
 
 const DashBoard = () => {
-  const { userName, budgets } = useLoaderData()
+  const { userName, budgets, expenses } = useLoaderData()
 
   return (
     <>
@@ -90,6 +92,16 @@ const DashBoard = () => {
                     <BudgetItem key={budget.id} budget={budget} />
                   ))}
                 </div>
+                {expenses && expenses.length > 0 && (
+                  <div className='grid-md'>
+                    <h2>Recent Expenses</h2>
+                    <Table
+                      expenses={expenses.sort(
+                        (a, b) => b.createdAt - a.createdAt
+                      )}
+                    />
+                  </div>
+                )}
               </div>
             ) : (
               <div className='grid-sm'>
